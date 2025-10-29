428d2a37cd54e5f7d983f8482cbb1c8c86d29c1b
Eduart2021 <zeedymarina@yahoo.com>
2025-10-29 11:20:07 -0500

    object mercury radius

--- planets.js
+++ planets.js
@@ -193,12 +193,13 @@ console.log(system.mercury.mass);
 console.log(system.venus.eccentricity);
 // 3. Console.log the sum of the number of satellites of Jupiter, Saturn, Uranus, and Neptune (131)
 console.log(system.jupiter.satellites);
-
-
 // 4. Write a conditional that checks if Mercury's radiusp is a string (remember the typeof command). If Mercury's radiusp is a string, change the value to be the same as its radiuse value. (radiusp is radius at the poles, radiuse is radius at the equator).
-
-
+if (typeof system.mercury.radiusp === "string") {
+  system.mercury.radiusp = system.mercury.radiuse;
+}
+console.log(system.jupiter.radiusp);
 // 5. Console.log Mercury's radiusp (should be 2439, not 'same').
+console.log(system.mercury.radiusp);
 
 
 // 6. Find the difference between Jupiter's momentum and Pluto's momentum. (To get momentum, you'll want the product of velocity and mass).
